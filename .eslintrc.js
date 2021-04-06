7d4f474a54564579cc4a43557db9bbfa97caa7f3
wuqianying <super571@sina.com>
2021-04-07 02:03:17 +0800

    feat: three

---
+++ .eslintrc.js
@@ -0,0 +1,25 @@
+module.exports = {
+    "globals": {
+        "requestAnimationFrame": false
+    },
+    "env": {
+        "browser": true,
+        "es2021": true
+    },
+    "extends": [
+        "eslint:recommended",
+        "plugin:react/recommended"
+    ],
+    "parserOptions": {
+        "ecmaFeatures": {
+            "jsx": true
+        },
+        "ecmaVersion": 12,
+        "sourceType": "module"
+    },
+    "plugins": [
+        "react"
+    ],
+    "rules": {
+    }
+};
